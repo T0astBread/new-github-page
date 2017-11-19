902030af1c41e3c039846e06b86687b9adae88c3
T0astBread <m.briedl@live.at>
2017-11-19 03:59:49 +0100

    Implemented some more stuff for animations
    Implemented in this commit: the backwards animation and different color animations (choosen randomly right now)
    
    I also added a library for browser detection since some of these features are very slow in some browsers (*cough cough*Firefox). (I know it's not good practice since it checks the User Agent I think, but I don't care.)

--- scripts/load-character-art.js
+++ scripts/load-character-art.js
@@ -13,18 +13,28 @@ let showArtwork = () =>
     $("#character-art-bg").show();
 }
 
-let showArtworkAnimated = () =>
+let showArtworkAnimated = (options={colorAnimationType:2, stepDelay:0, backwards:true}) =>
 {
-    console.log("show");
+    let stepSize = $("html").is(".gecko") ? 90 : 20;  // To compensate for Firefox's slow JS engine
+
+    let bg = $("#character-art-bg");
+    bg.width(bg.width());
+
     let artworkParts = $("#character-art-bg pre font");
-    console.log(artworkParts);
-    artworkParts.hide();
+    artworkParts.each((i, part) => part.setAttribute("tb-temp-color", part.getAttribute("color")));
+    artworkParts.attr("color", "white").hide();
 
     let showArtworkParts = (index, amount=1) =>
     {
-        console.log("show " + index);
-        for(let i = 0; i < amount; i++) artworkParts.eq(index++).show();
-        if(index < artworkParts.length - 1) setTimeout(() => showArtworkParts(index, 5), 1);
+        for(let i = 0; i < amount; i++)
+        {
+            let part = artworkParts.eq((options.backwards ? artworkParts.length - index : index));
+            index++;
+            part.show();
+            if(options.colorAnimationType === 1) part.attr("color", part.attr("tb-temp-color"));
+        }
+        if(index < artworkParts.length - 1) setTimeout(() => showArtworkParts(index, stepSize), options.stepDelay);
+        else if(options.colorAnimationType === 2) artworkParts.each((i, part) => part.setAttribute("color", part.getAttribute("tb-temp-color")));
     }
     showArtworkParts(0);
 }
