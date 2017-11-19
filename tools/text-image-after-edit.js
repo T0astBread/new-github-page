4f3ab04491994153bbff0df6b4361cc72ab825f3
T0astBread <m.briedl@live.at>
2017-11-19 02:46:05 +0100

    Implemented special character artwork BG thing
    This includes the old unused and the current implementation. I will remove the old one in a future commit.
    
    This also includes some other minor things but I'm too lazy to split them into different commits.

---
+++ tools/text-image-after-edit.js
@@ -0,0 +1,7 @@
+//Script that makes white background on http://www.text-image.com transparent
+
+$$("font[color=white]").forEach(i =>
+{
+    i.setAttribute("color", "#eab");
+    i.style.color = "transparent";
+});
